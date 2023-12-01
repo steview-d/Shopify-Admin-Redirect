867b9b0245ce4825ab685bc6a81629a8c04eaa0d
Dave Laffan <dave.laffan@outlook.com>
2023-12-01 12:29:15 +0000

    Update to include blog pages & policies

--- url_builder.js
+++ url_builder.js
@@ -1,8 +1,16 @@
-const paths = ['/products/', '/collections/', '/pages/'];
+const paths = [
+  '/products/',
+  '/collections/',
+  '/pages/',
+  '/blogs/',
+  '/policies/',
+];
 const isMatch = paths.some(path => window.location.pathname.includes(path));
 
 if (isMatch && window.Shopify) {
   let adminUrlElement = document.createElement('admin-url');
-  adminUrlElement.textContent = `${window.Shopify.shop}/admin/${meta.page.pageType}s/${meta.page.resourceId}`;
+  window.location.pathname.includes('/policies/')
+    ? (adminUrlElement.textContent = `${window.Shopify.shop}/admin/settings/legal`)
+    : (adminUrlElement.textContent = `${window.Shopify.shop}/admin/${meta.page.pageType}s/${meta.page.resourceId}`);
   document.head.appendChild(adminUrlElement);
 }
